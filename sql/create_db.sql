96f50e4ac124531286e5d442d45959598b78e2fa
Giulio Bosco <giuliobva@gmail.com>
2018-10-22 14:17:17 +0200

    fix create_db - doc

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,21 +1,36 @@
+/*
+ * If the database users already exist remove it.
+ */
 DROP DATABASE IF EXISTS users;
 
+/*
+ * Create the users database.
+ */
 CREATE DATABASE users;
 
+/*
+ * Create the user table.
+ */
 CREATE TABLE users.user (
 	username VARCHAR(50) PRIMARY KEY,
 	password VARCHAR(200),
 	name VARCHAR(200)
 );
 
---
--- DEFINE STORED PROCEDURES
---
+/*
+ * ---------------------------------------------------------------------------------------------------------- procedures
+ */
 
+/*
+ * Check user procedure.
+ */
 DELIMITER //
 CREATE PROCEDURE is_valid_user(IN username varchar(64))
 	BEGIN
 		SELECT username, name FROM users.user WHERE username LIKE user.username;
 	END // DELIMITER ;
 
+/*
+ * --------------------------------------------------------------------------------------------------------- insert data
+ */
 INSERT INTO users.user (username, password, name) VALUES ('root', 'root', 'Root');
