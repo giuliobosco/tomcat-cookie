cba697be667c430d3228e7f7837f764680a115dd
Giulio Bosco <giuliobva@gmail.com>
2018-10-22 14:12:33 +0200

    fix create_db.sql procedure

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,7 +1,21 @@
-CREATE DATABASE users;
+CREATE DATABASE IF NOT EXISTS users;
+
+DROP TABLE IF EXISTS users.user;
 
 CREATE TABLE users.user (
 	username VARCHAR(50) PRIMARY KEY,
 	password VARCHAR(200),
 	name VARCHAR(200)
 );
+
+--
+-- DEFINE STORED PROCEDURES
+--
+
+DELIMITER //
+CREATE PROCEDURE is_valid_user(IN username varchar(64))
+	BEGIN
+		SELECT username, name FROM users.user WHERE username LIKE user.username;
+	END // DELIMITER ;
+
+INSERT INTO users.user (username, password, name) VALUES ('root', 'root', 'Root');
